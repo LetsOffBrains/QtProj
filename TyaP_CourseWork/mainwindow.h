885d4a79d5a1248c935d5484b5d812ad5095cae7
Krasilnikov Anton <darkhorseaka@ya.ru>
2018-12-11 12:53:41 +0700

    cw update still not working

--- TyaP_CourseWork/mainwindow.h
+++ TyaP_CourseWork/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QDebug>
 #include <QMessageBox>
+#include <QMap>
 
 namespace Ui {
 class MainWindow;
@@ -20,13 +21,25 @@ public:
     QStringList forbiden;
     QString separator;
 
-	QStringList vTerm, blockRes;
-	QString toRepeat, subString, blockRegEx;
+	QStringList vTerm;
+	QString toRepeat, subString;
     int toMul;
+
+	QStringList oldTerm;
+	QString oldToRepeat, oldSubString;
+	int oldToMul;
+
+	QMap<QString, QStringList> mapBlocks;
+	QMap<QString, QStringList> mapSave;
+	QMap<QString, int> mapPot;
+
 public slots:
 	void on_aTheme_activated();
 	void on_aAuthor_activated();
 	void on_aSave_activated();
+
+	bool check_RegEx(QString RegEx);
+	void split_RegEx(QString RegEx, QString parent);
 
 private slots:
     void on_leTerm_editingFinished();
@@ -42,6 +55,8 @@ private slots:
 	void on_pbCreateReg_clicked();
 
 	void on_leSubString_returnPressed();
+
+	void on_pbOutput_clicked();
 
 private:
     Ui::MainWindow *ui;
